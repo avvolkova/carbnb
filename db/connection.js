1993fd83fadc77387dfb506be3fdbc3b28c49bba
Alexandr <andrashov1@gmail.com>
2021-01-14 14:05:17 +0300

    .BD server

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,17 @@
-const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/carbnb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+ const mongoose = require('mongoose');
+ const Article = require("../models/article");
+const User = require("../models/user");
+
+
+ mongoose.connect('mongodb+srv://Alexandr:12345@cluster0.2ixnd.mongodb.net/carBnB?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+
+ 
+ const user = new User ({name:'VANYA' })
+ const user2= new User({name:"Kolya"})
+ user.save()
+ user2.save()
+// const diet1 = new Diet({ name: "Медоедская" });
