7205d374a29542f9365221b2c79a655829673ea3
Gamz <jeydey@mail.ru>
2021-01-14 20:02:54 +0300

    теперь авторизация работает

--- routes/users.js
+++ routes/users.js
@@ -21,11 +21,21 @@ router.post("/signup", async function (req, res) {
     isCarOwner: false,
   });
   await newUser.save();
-  res.render(`profile`, { newUser });
+  res.redirect(`/users/${newUser._id}`);
 });
 
-// router.post("/login", function (req, res, next) {
-//   res.render("login");
-// });
+router.post("/login", async function (req, res, next) {
+  let username = req.body.nameUser;
+  let userDB = await UserSchema.findOne({ email: username });
+  let id = userDB._id;
+  console.log(userDB);
+  res.redirect(`/users/${userDB._id}`);
+});
+
+router.get("/:id", async function (req, res, next) {
+  let id = req.params.id;
+  let user = await UserSchema.findById(id);
+  res.render("profile", { user });
+});
 
 module.exports = router;
