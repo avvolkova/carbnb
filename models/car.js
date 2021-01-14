e016dc846b3017fcb7967cededd9b3b4be439982
Alexandr <andrashov1@gmail.com>
2021-01-14 11:02:29 +0300

    .Schema User,Car

---
+++ models/car.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+  },
+  registration: {
+    type: Date,
+    required: true,
+  },
+  isCarOwner: {
+    type: Boolean,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
+
+// 1. Имя 
+// 2. Фотография
+// 3. Дата регистрации
+// 4. Является ли владельцем(если да,то)
+
+// 5. Ссылка на объявление пользователя
+// 6. Ссылка на отзывы
---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose =require('mongoose');
+const user = require('./user');
+
+const ArticleSchema = new mongoose.Schema({
+  nameAuto:{
+    type:String,
+    required:true,
+  },
+  img: {
+    type: String,
+  },
+  carOwner: {
+    type: mongoose.Schema.Types.ObjectId, 
+    required: true,
+    ref:"User",
+  },
+  transmission: {
+    type: String, 
+    // required: true,
+  },
+  yearAuto:{
+  type:Number,
+  required:true,
+  },
+  mileage:{
+  type:String
+  },
+  isActive: {
+    type: Boolean,
+  },
+});
+
+module.exports = mongoose.model('Article', ArticleSchema);
+
+// 1. Название авто
+// 2. Фотографии от владельцев
+// 3. Ссылка на владельца авто
+// 4. Трансмиссия
+// 5. Год производства
+// 6. Пробег
