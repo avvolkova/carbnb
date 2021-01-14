01c05110b40a3c7149fbfee43d1d8092110476cd
Gamz <jeydey@mail.ru>
2021-01-14 17:20:33 +0300

    ---

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,11 @@
  const mongoose = require('mongoose');
- const Article = require("../models/article");
-const User = require("../models/user");
 
 
- mongoose.connect('mongodb+srv://Alexandr:12345@cluster0.2ixnd.mongodb.net/carBnB?retryWrites=true&w=majority', {
+
+ module.exports = mongoose.connect('mongodb+srv://Alexandr:12345@cluster0.2ixnd.mongodb.net/carBnB?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 
  
- const user = new User ({name:'VANYA' })
- const user2= new User({name:"Kolya"})
- user.save()
- user2.save()
-// const diet1 = new Diet({ name: "Медоедская" });
--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,24 @@ const express = require("express");
 const router = express.Router();
 const UserSchema = require("../models/user");
 
-
 /* клик по кнопки регистрации. */
 router.get("/login", function (req, res, next) {
   res.render("login");
 });
 
+
+router.get("/signup", function (req, res, next) {
+  res.render("signup");
+});
+
 /* добавление юзера в базу */
-router.post("/login", async function (req, res) {
+router.post("/signup", async function (req, res) {
+  console.log(req.body);
   let newUser = new UserSchema({
-    name: req.body.value,
-    registration: req.body.value,
-    isCarOwner: req.body.value,
+    name: req.body.name,
+    password: req.body.password,
+    email: req.body.email,
+    isCarOwner: false,
   });
   await newUser.save();
   res.redirect(`/profile/${newUser.id}`);
