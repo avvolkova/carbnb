daa8790deda2ba2dcff6d49fa617d9c5eb685028
Anna <79162783781@yandex.ru>
2021-01-14 17:54:09 +0300

    Гл.страница с поиском + немного авторизации

--- app.js
+++ app.js
@@ -3,18 +3,13 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-<<<<<<< HEAD
 const hbs = require('hbs');
 const connection = require('./db/connection')
-=======
-const hbs = require("hbs");
-const connection = require("./db/connection");
->>>>>>> origin/gamzat
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const serviceRouter = require("./routes/service");
-
+const session = require('express-session');
 
 const app = express();
 
@@ -22,6 +17,20 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
+
+app.use(session({
+    secret: '45454545', // случайный набор символов для шифрования сессионных куков
+    resave: true, // пересохранять сессию даже если ничего не изменилось
+    saveUninitialized: true, // сохранять сессию при первом обращении к сайту
+    cookie: { secure: false, maxAge: 1000000000000000 }, // опции сессонных куков ( secure - это httpS )
+}));
+
+app.use((req, res, next) => {
+    if (req.session.userID) {
+        res.locals.userID = req.session.userID // записываем в локалс имя юзера из сессии
+    }
+    next();
+});
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -35,19 +44,19 @@ app.use("/service", serviceRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -3,26 +3,25 @@ const router = express.Router();
 const UserSchema = require("../models/user");
 
 /* клик по кнопки регистрации. */
-router.get("/login", function (req, res, next) {
-  res.render("login");
+router.get("/login", function(req, res, next) {
+    res.render("login");
 });
 
-
-router.get("/signup", function (req, res, next) {
-  res.render("signup");
+router.get("/signup", function(req, res, next) {
+    res.render("signup");
 });
 
 /* добавление юзера в базу */
-router.post("/signup", async function (req, res) {
-  console.log(req.body);
-  let newUser = new UserSchema({
-    name: req.body.name,
-    password: req.body.password,
-    email: req.body.email,
-    isCarOwner: false,
-  });
-  await newUser.save();
-  res.redirect(`/profile/${newUser.id}`);
+router.post("/signup", async function(req, res) {
+    console.log(req.body);
+    let newUser = new UserSchema({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        isCarOwner: false,
+    });
+    await newUser.save();
+    res.redirect(`/profile/${newUser.id}`);
 });
 
 module.exports = router;
