589081a996ab400acb423ef4c0a9ea16a9af5e69
Anna <79162783781@yandex.ru>
2021-01-14 14:35:37 +0300

    gitignore + header

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* перенаправляет на главную страницу */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.redirect('/service');
 });
 
 module.exports = router;
