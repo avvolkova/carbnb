6937cb906a86962696267f6a67b397946ed96abe
Gamz <jeydey@mail.ru>
2021-01-14 19:01:16 +0300

    a

--- db/connection.js
+++ db/connection.js
@@ -1,18 +1,22 @@
- const mongoose = require('mongoose');
- const Article = require('../models/article');
+const mongoose = require("mongoose");
+const Article = require("../models/article");
 
- module.exports = mongoose.connect('mongodb+srv://Alexandr:12345@cluster0.2ixnd.mongodb.net/carBnB?retryWrites=true&w=majority', {
-     useNewUrlParser: true,
-     useFindAndModify: true,
-     useUnifiedTopology: true,
-     useFindAndModify: false
- });
+module.exports = mongoose.connect(
+  "mongodb+srv://Alexandr:12345@cluster0.2ixnd.mongodb.net/carBnB?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
 
- //  const auto = new Article({
- //      nameAuto: 'mazda',
- //      transmission: 'gg',
- //      yearAuto: 2012,
- //      mileage: '340000',
- //      isActive: true,
- //      city: "Moscow"
- //  }).save(
+// const auto = new Article({
+//   nameAuto: "bugatti",
+//   transmission: "auto",
+//   yearAuto: 2021,
+//   mileage: "5000",
+//   isActive: true,
+//   city: "Moscow",
+//   carOwner: "60004818eea1f1774b452abf",
+// }).save();
