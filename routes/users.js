8a9eab6356bdb7b6ad6e6ca5608258ec81258594
Gamz <jeydey@mail.ru>
2021-01-14 14:42:34 +0300

    Создал два роута

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const UserSchema = require("../models/user");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+/* клик по кнопки регистрации. */
+router.get("/login", function (req, res, next) {
+  res.render("login");
+});
+
+/* добавление юзера в базу */
+router.post("/login", async function (req, res) {
+  let newUser = new UserSchema({
+    name: req.body.value,
+    registration: req.body.value,
+    isCarOwner: req.body.value,
+  });
+  await newUser.save();
+  res.redirect(`/profile/${newUser.id}`);
 });
 
 module.exports = router;
