68a0dae20ed79262e8ffdbd4cb2655387cc374a0
Alexandr <woonerwafl1@gmail.com>
2021-01-15 16:18:55 +0300

    add photo

--- routes/create.js
+++ routes/create.js
@@ -1,6 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const ArticleSchema = require("./article");
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+const path = require('path')
+
+const appRootDir = path.dirname(require.main.filename).replace('/bin', '')
+
+// const ArticleSchema = require("./article");
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, path.join(appRootDir, 'public', 'img'))
+  },
+  filename(req, file, cb) {
+    cb(null, `img-${Date.now()}${path.extname(file.originalname)}`)
+  },
+})
+
+const uploadMulter = multer({
+  storage,
+  fileFilter(req, file, cb) {
+    const filetypes = /jpeg|jpg|png|svg/
+    const mimetype = filetypes.test(file.mimetype)
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+
+    if (mimetype && extname) {
+      return cb(null, true)
+    }
+    return cb(null, false)
+  },
+})
 
 /* создание объявления */
 router.get("/:id", function (req, res) {
@@ -13,3 +43,13 @@ router.get("/:id", function (req, res) {
     isActive: req.body.value,
   };
 });
+
+router.get("/", function (req, res) {
+  res.render('create')
+});
+
+router.post('/new', uploadMulter.single('carImg'), function (req, res, next) {
+  res.redirect('/');
+})
+
+module.exports = router;
