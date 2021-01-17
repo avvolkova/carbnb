f7e848f8478319e774ba193adac6defcbfa8d57c
Anna <79162783781@yandex.ru>
2021-01-17 15:39:46 +0300

    Фиксация изменений. Почему-то не работает регистрация

--- routes/users.js
+++ routes/users.js
@@ -4,60 +4,69 @@ const router = express.Router();
 const UserSchema = require("../models/user");
 /* функция для разлогирования */
 const check = (req, res, next) => {
-  console.log(req.session);
-  if (req.session.userID) {
-    next();
-  } else {
-    res.redirect("/");
-  }
+    console.log(req.session);
+    if (req.session.userID) {
+        next();
+    } else {
+        res.redirect("/");
+    }
 };
 
-
-
-/* клик по кнопке регистрации. */
+/* авторизация */
 router
-  .route("/login")
-  .get(function (req, res, next) {
-    res.render("login");
-  })
-  .post(async function (req, res, next) {
-    let username = req.body.nameUser;
-    let userDB = await UserSchema.findOne({ name: username });
-    let id = userDB._id;
-    req.session.userID = id;
-    req.session.username = userDB.name;
-    res.redirect(`/`);
-  });
-
-router
-  .route("/signup")
-  .get(function (req, res, next) {
-    res.render("signup");
-  })
-  .post(async function (req, res) {
-    console.log(req.body);
-    let newUser = new UserSchema({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-      isCarOwner: false,
+    .route("/login")
+    .get(function(req, res, next) {
+        res.render("login");
+    })
+    .post(async function(req, res, next) {
+        let { username, password } = req.body;
+        let userDB = await UserSchema.findOne({ name: username });
+        if (userDB) {
+            if (password === userDB.password) {
+                req.session.userID = userDB._id;
+                req.session.username = userDB.name;
+                res.redirect(`/`);
+            } else {
+                res.render('login', { wrongPassword: true });
+            }
+        } else {
+            res.render('login', { notFound: true });
+        }
     });
-    await newUser.save();
-    req.session.userID = newUser._id;
-    req.session.username = newUser.name;
-    res.redirect(`/`);
-  });
 
-/* роутер разлогирования */
-router.get("/logout", async function (req, res) {
-  await req.session.destroy();
-  res.redirect("/service");
+/* регистрация */
+
+router.get('/signup', function(req, res, next) {
+    res.render("signup");
+})
+
+router.post('/signup', async function(req, res) {
+    if (req.body) {
+        let newUser = new UserSchema({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email,
+            isCarOwner: false,
+            img: '../public/img/user-avatar.jpg',
+        });
+        await newUser.save();
+        console.log(newUser);
+        req.session.userID = newUser._id;
+        req.session.username = newUser.name;
+        res.redirect(`/`);
+    }
 });
 
-router.get("/:id", check, async function (req, res, next) {
-  let id = req.params.id;
-  let user = await UserSchema.findById(id);
-  res.render("profile", { user });
+/* роутер разлогирования */
+router.get("/logout", async function(req, res) {
+    await req.session.destroy();
+    res.redirect("/service");
+});
+
+router.get("/:id", check, async function(req, res, next) {
+    let id = req.params.id;
+    let user = await UserSchema.findById(id);
+    res.render("profile", { user });
 });
 
 module.exports = router;
